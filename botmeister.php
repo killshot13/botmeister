9bf62743f32c47673654b9ea01bd82c7d4bbc1e9
killshot13 <dmreh@outlook.com>
2020-10-18 12:14:57 -0500

    minor fixes to licensing header in source file

--- botmeister.php
+++ botmeister.php
@@ -2,10 +2,10 @@
 /**
  * Plugin Name
  *
- * @package      Botmeister\PluginSlug
- * @author       Michael Rehnert
- * @copyright    2020 Safe This Home, LLC
- * @license      GPL-2.0+
+ * package      Botmeister\PluginSlug
+ * author       Michael Rehnert
+ * copyright    2020 Safe This Home, LLC
+ * license      GPL-2.0+
  *
  * @wordpress-plugin
  * Plugin Name:       Botmeister
@@ -23,18 +23,24 @@
  */
 
  /*
-Botmeister is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-any later version.
+	BOTMEISTER: A WordPress plugin for combating comment spam generated
+	on posts & articles by bots.
 
-Botmeister is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
+    Copyright (C) 2020  Safe This Home, LLC a/o Michael Rehnert
 
-You should have received a copy of the GNU General Public License
-along with Botmeister. If not, see http://www.gnu.org/licenses/gpl-2.0.txt.
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
 if(!class_exists('Botmeister'))
