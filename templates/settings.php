3266ebc149a851d958e0aa8ab63f26234dc2618d
killshot13 <dmreh@outlook.com>
2020-10-31 15:06:42 -0500

    minor bug fixes and filename changes.

--- templates/settings.php
+++ templates/settings.php
@@ -1,5 +1,5 @@
 <div class="wrap">
-    <h2>WP Plugin Template</h2>
+    <h2>Botmeister</h2>
     <form method="post" action="options.php">
         <?php @settings_fields('botmeister-group'); ?>
         <?php @do_settings_fields('botmeister-group'); ?>
